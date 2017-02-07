c0dfb6d555eada327010bea9ac06d0a4b8f1607e
Gricel Dayhanna Sepúveda Rozas <gr3c2l@gmail.com>
2017-02-07 11:41:09 -0300

    Lot-of-fixes

--- config.rb
+++ config.rb
@@ -1,16 +1,16 @@
 
-css_dir = "css" # by Fire.app 
-sass_dir = "scss" # by Fire.app 
-images_dir = "images" # by Fire.app 
-javascripts_dir = "javascripts" # by Fire.app 
-fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
-fireapp_livescripts_dir = "livescripts" # by Fire.app 
-fireapp_minifyjs_on_build = false # by Fire.app 
-fireapp_always_report_on_build = true # by Fire.app 
-output_style = :expanded # by Fire.app 
-relative_assets = false # by Fire.app 
-line_comments = true # by Fire.app 
-sass_options = {:debug_info=>false} # by Fire.app 
-sourcemap = false # by Fire.app 
-fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
-fireapp_livescript_options = {:bare=>false} # by Fire.app 
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_livescripts_dir = "livescripts"
+fireapp_minifyjs_on_build = false
+fireapp_always_report_on_build = true
+output_style = :expanded
+relative_assets = false
+line_comments = true
+sass_options = {:debug_info=>false}
+sourcemap = false
+fireapp_coffeescript_options = {:bare=>false}
+fireapp_livescript_options = {:bare=>false}
